c0f910daacc0e881a6c90202fd48633bf26d1ee9
Emre Elagoz <emreelagoz1994@gmail.com>
2020-10-16 09:47:55 +0200

    multiarray and entering values via console readline

--- MultiDimensionalArrays/Program.cs
+++ MultiDimensionalArrays/Program.cs
@@ -10,7 +10,53 @@ namespace MultiDimensionalArrays
     {
         static void Main(string[] args)
         {
+            #region initialize and for
+            ////int[] array = {1, 3, 9, 5} single dim array
+            ////int[,] arrayMulti = new int[3, 4] multidimensional array
+            ////Console.WriteLine(array[2, 0]);
 
+            //int[,] multiArray = new int[2, 2];
+
+            //multiArray[0, 0] = 30;
+            //multiArray[0, 1] = 60;
+            //multiArray[1, 0] = 90;
+            //multiArray[1, 1] = 120;
+
+            //for (int i = 0; i < 2; i++)
+            //{
+            //    for (int j = 0; j < 2; j++)
+            //    {
+            //        Console.WriteLine(multiArray[i, j] + "\t");
+            //    }
+            //    Console.WriteLine();
+            //}
+            #endregion
+            
+            
+            int[,] arrayMulti = new int[2, 2];
+
+            #region entering values via consolereadline
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    Console.WriteLine($"Enter element into position {i} {j}");
+                    arrayMulti[i, j] = int.Parse(Console.ReadLine());
+
+                }
+            }
+
+            Console.WriteLine("You entered the following values");
+
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    Console.WriteLine(arrayMulti[i,j] + "\t");
+                }
+                Console.WriteLine();
+            }
+            #endregion
         }
     }
 }
