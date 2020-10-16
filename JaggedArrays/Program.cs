bc2e7fa4181ffb4587f7def644d544157ee32dbf
Emre Elagoz <emreelagoz1994@gmail.com>
2020-10-16 11:20:31 +0200

    Jagged and multidimensional arrays

---
+++ JaggedArrays/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JaggedArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[][] jaggedArray = new int[3][];
+
+            jaggedArray[0] = new int[] { 1, 8, 9 };
+            jaggedArray[1] = new int[] { 4, 6, 4 };
+            jaggedArray[2] = new int[] { 5, 6, 7, 8, 9, 3 };
+
+            for (int i = 0; i < jaggedArray.Length; i++)
+            {
+                for (int j = 0; j < jaggedArray[i].Length; j++)
+                {
+                    Console.Write(jaggedArray[i][j] + "\t");
+                }
+                Console.WriteLine();
+            }
+            
+        }
+    }
+}
